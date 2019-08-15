4d05110251edf40be3b9df1cc4119f7853b9798b
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-15 20:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "Susan") {
-  console.log("The name is correct")
-}
-
-else {
-  console.log("Expect the name to be Susan, it's Joe")
-}
-
 // Don't worry about this
 module.exports = { name, height
 }
-
-
